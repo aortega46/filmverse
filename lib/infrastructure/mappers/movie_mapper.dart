3a6753408202deb49ed4aaafd85f6d7235a41a88
Albert Ortega <trebla.og46@gmail.com>
2024-11-10 11:21:51 +0100

    feat: :sparkles: movie details mapper

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,5 +1,5 @@
 import 'package:filmverse/domain/entities/movie.dart';
-import 'package:filmverse/infrastructure/models/moviedb/movie_moviedb.dart';
+import 'package:filmverse/infrastructure/models/models.dart';
 
 class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
@@ -21,5 +21,26 @@ class MovieMapper {
         video: moviedb.video,
         voteAverage: moviedb.voteAverage,
         voteCount: moviedb.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails movie) => Movie(
+        adult: movie.adult,
+        backdropPath: movie.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${movie.backdropPath}'
+            : 'https://www.prokerala.com/movies/assets/img/no-poster-available.jpg',
+        genreIds: movie.genres.map((e) => e.name).toList(),
+        id: movie.id,
+        originalLanguage: movie.originalLanguage,
+        originalTitle: movie.originalTitle,
+        overview: movie.overview,
+        popularity: movie.popularity,
+        posterPath: movie.posterPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
+            : 'https://www.prokerala.com/movies/assets/img/no-poster-available.jpg',
+        releaseDate: movie.releaseDate,
+        title: movie.title,
+        video: movie.video,
+        voteAverage: movie.voteAverage,
+        voteCount: movie.voteCount,
       );
 }
